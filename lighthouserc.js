d6d8700baa4fa7d9873e5e3962539ef4989eb5c6
Alex Rosso <alexrosso1025@gmail.com>
2024-11-22 16:22:45 -0500

    Docker configutarion

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const fs = require('fs');
+
 
 module.exports = {
   ci: {
@@ -9,11 +11,20 @@ module.exports = {
       chromePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
       puppeteerScript: "./set-cookie.js",
     },
+    server: {
+        basicAuth:{
+            username: "username",
+            password: "test"
+        },
+    },
     assert: {
       preset: "lighthouse:recommended",
     },
     upload: {
-      target: "temporary-public-storage",
+      //target: "temporary-public-storage",
+      target: 'lhci',
+      serverBaseUrl: 'http://localhost:9001/',
+      token: '32ccc138-f82c-4163-9e38-3cf1fbbd2a74'
     },
   },
 };
